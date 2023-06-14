11c46034ffe267ea59865492713b8dd0d5c6d1f6
SeraphimKa <fandomethor@gmail.com>
2023-06-14 14:59:22 +0300

    nav tweak

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,11 +1,12 @@
 
 <script setup lang="ts">
 import { RouterLink } from 'vue-router'
+
 </script>
 <template>
     <header>
-        <router-link to="/">MyTodo</router-link>
-        <router-link to="/notes">MyNote</router-link>
+        <router-link to="/" active-class="active">MyTodo</router-link>
+        <router-link to="/notes" active-class="active">MyNote</router-link>
     </header>
 </template>
 
@@ -17,11 +18,18 @@ header {
 
 a {
     font-family: 'Gill Sans', 'Gill Sans MT', Calibri, 'Trebuchet MS', sans-serif;
-    text-decoration-line: underline;
     font-weight: 600;
     color: rgb(0, 0, 0);
-    text-shadow: 0 0 3px rgb(255, 255, 255);
+    text-shadow: 0 0 2px rgb(255, 255, 255);
     font-size: 2rem;
 }
 
+a:hover {
+    color: rgb(25, 25, 25)
+}
+
+.active {
+    text-shadow: 0 0 5px rgb(255, 255, 255);
+    text-decoration-line: underline;
+}
 </style>
